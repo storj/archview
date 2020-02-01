3475e4fdb241ff456452cef76ec3275cb61f1f5b
Egon Elbre <egonelbre@gmail.com>
2020-02-01 20:31:36 +0200

    add utility funcs

--- edit/classes.go
+++ edit/classes.go
@@ -4,6 +4,17 @@ import (
 	"github.com/storj/archview/arch"
 )
 
+// ComponentsWithClass returns list of components with the  class.
+func ComponentsWithClass(graph *arch.World, classes []string) []*arch.Component {
+	var components []*arch.Component
+	for _, component := range graph.Components {
+		if contains(classes, component.Class) {
+			components = append(components, component)
+		}
+	}
+	return components
+}
+
 // RemoveClasses removes any components that has class in the specified list.
 func RemoveClasses(graph *arch.World, classes []string) {
 	var keep []*arch.Component
--- edit/roots.go
+++ edit/roots.go
@@ -4,6 +4,25 @@ import (
 	"github.com/storj/archview/arch"
 )
 
+// Roots returns components without incoming links.
+func Roots(world *arch.World) []*arch.Component {
+	parents := map[*arch.Component]int{}
+	for _, comp := range world.Components {
+		parents[comp] += 0
+		for _, link := range comp.Links {
+			parents[link.Target]++
+		}
+	}
+
+	var roots []*arch.Component
+	for comp, count := range parents {
+		if count == 0 {
+			roots = append(roots, comp)
+		}
+	}
+	return roots
+}
+
 // KeepRoots keeps any component that is a dependency to root.
 func KeepRoots(world *arch.World, roots []string) {
 	walked := map[*arch.Component]bool{}
